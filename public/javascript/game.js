3714c701639e27da739abb30524655a4f0d7853e
Drew Mitchell <hi@callmedrew.com>
2017-02-19 02:54:12 -0500

    Add page for websocket client.

---
+++ public/javascript/game.js
@@ -0,0 +1,46 @@
+var webSocket;
+var messages = document.getElementById("chat");
+
+function addMessageToChat(text) {
+	messages.innerHTML += "<br/>" + text;
+	messages.scrollTop = messages.scrollHeight;
+}
+
+function joinGame() {
+	if (webSocket !== undefined && webSocket.readyState !== WebSocket.CLOSED) {
+		addMessageToChat("WebSocket is already opened.");
+		return;
+	}
+	// Create a new instance of the websocket
+	var url = "ws://" + window.location.host + "/socket";
+	webSocket = new WebSocket(url);
+
+	/** Binds functions to the listeners for the websocket */
+	webSocket.onopen = function(e) {
+		if (e.data === undefined){
+			return;
+		}
+		addMessageToChat(e.data);
+	};
+
+	/** Handle messages that are received from the server */
+	webSocket.onmessage = function(e) {
+		addMessageToChat(e.data);
+	};
+
+	/** Handle closing the connection */
+	webSocket.onclose = function(e) {
+		addMessageToChat("Connection closed.");
+	};
+}
+
+
+function sendChatMessage() {
+	var text = document.getElementById("chatInput").value;
+	document.getElementById("chatInput").value = "";
+	if (text != "") {
+		webSocket.send(text);
+	}
+}
+
+window.onload = joinGame
