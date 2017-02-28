f6fe7b2bd30a8c3976b4c78826e660d953993559
Drew Mitchell <hi@callmedrew.com>
2017-02-27 22:49:02 -0500

    Add layout pages for home page and mode selection pages.

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Turing Party' });
 });
 
-/* GET home page. */
+/* Mode selection page. */
+router.get('/select', function(req, res, next) {
+  res.render('select');
+});
+
+/* Page to start a new room with friends. */
+router.get('/start-room', function(req, res, next) {
+  res.render('start-room');
+});
+
+/* Page to join a room of friends. */
+router.get('/join-room', function(req, res, next) {
+  res.render('join-room');
+});
+
+/* Page to join a random room. */
+router.get('/join-random', function(req, res, next) {
+  res.render('join-random');
+});
+
+/* Gameplay page. */
 router.get('/game', function(req, res, next) {
   res.render('game', { title: 'Turing Party' });
 });
