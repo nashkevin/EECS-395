c690a848de923f48d89096e09dd00ebb81af8cd1
ianlav <irl@case.edu>
2017-03-06 00:09:32 -0500

    Adds bots to the game, with full representation

--- game/room.js
+++ game/room.js
@@ -1,8 +1,17 @@
 // Class definition for a Room.
 
+var ConversationV1 = require('watson-developer-cloud/conversation/v1');
+
+var conversation = new ConversationV1({
+  "username": "a43eee8f-6a96-40fb-a297-f99365f2c202",
+  "password": "uX8MIl44vAmx",
+	"version_date": '2017-02-03'
+});
+
 const WebSocket = require('ws');
 
 const MAX_SIZE = 2; //TODO this is for testing
+const NUM_BOTS = 1;
 
 var method = Room.prototype;
 
@@ -16,6 +25,8 @@ function Room(maxSize) {
         this.maxSize = MAX_SIZE;
     }
 
+    this.numBots = NUM_BOTS;
+
     // Each player will receive an ID between 0 and (size-1) upon joining the
     // room. The IDs will be in random order, so that the first player to join
     // isn't always player 0.
@@ -25,6 +36,11 @@ function Room(maxSize) {
     }
     // Shuffle. https://css-tricks.com/snippets/javascript/shuffle-array/
     this._remainingPlayerIds.sort(function() {return 0.5 - Math.random()});
+
+    this.botIDs = [];
+    for (var i=this.maxSize; i<this.maxSize + this.numBots; i++) {
+        this.botIDs.push(i);
+    }
 
     this._playerToId = new WeakMap();
 }
@@ -48,10 +64,16 @@ method.isFull = function() {
     return (this.members.size >= this.maxSize);
 }
 
-method.broadcast = function(message, sender) {
+method.broadcast = function(message, sender, botID = -1, botResponse = true) {
     var payload = message;
     if (sender) {
-        var id = this._playerToId.get(sender);
+        var id;
+        if(botID == -1) {
+          id = this._playerToId.get(sender);
+        }
+        else {
+          id = botID;
+        }
         payload = JSON.stringify({"playerMessage": {"id": id, "message": message}});
     }
     this.members.forEach(function each(client) {
@@ -59,6 +81,29 @@ method.broadcast = function(message, sender) {
 			client.send(payload);
 		}
 	});
+
+  if(botResponse) {
+    var bot = this.botIDs[0]; //will be replaced by the above botID
+    var mems = this.members;
+    setTimeout(function() {
+			var answer = conversation.message({
+				workspace_id: 'd4c1a5ce-0485-4c20-b593-2e62b1bc5319',
+		 		input: {'text': message},
+		 		context: {}
+			},  function(err, response) {
+			  if (err)
+			    console.log('error:', err);
+			  else {
+					var payload = JSON.stringify({"playerMessage": {"id": bot, "message": JSON.stringify(response.output.text).slice(2, -2)}});
+					mems.forEach(function each(client) {
+		  		  if (client.readyState === WebSocket.OPEN) {
+		  		    client.send(payload);
+		  		  }
+		  	  });
+				}
+			});
+    }, 2000);
+  }
 }
 
 /* Sends a message to each client that gameplay is starting. Also tell
